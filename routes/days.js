b63a2b732151ff3555cdfde80d15094e9285cf7b
ianH92 <>
2017-12-28 00:33:39 -0800

    added days controller and routes

---
+++ routes/days.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var daysController = require('../controllers/daysController.js');
+
+router.get('/:year/:month/:day', daysController.dayDisplay);
+
+module.exports = router;
