82cf7172bc3c7997e900e394441bc9d2e1bd174f
ianH92 <>
2017-12-31 22:53:47 -0800

    added createEvent

--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var eventController = require('../controllers/eventController.js');
 
+router.get('/createEvent', eventController.createEvent);
+
 router.get('/:eventId', eventController.eventDisplay);
 
 module.exports = router;
