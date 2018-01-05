86164112238e3ec9a118097a49fe675509e8d360
ianH92 <>
2018-01-04 21:08:16 -0800

    added data validation and sanitization using express-validator

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,4 +1,6 @@
 var Event = require('../models/Event.js');
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 exports.eventDisplay = function(req, res) {
 
@@ -20,28 +22,37 @@ exports.createEvent = function(req, res) {
 	res.render('createEvent');
 }
 
-exports.createEventPost = function(req, res) {
+exports.createEventPost = [
+	body('name', 'Name for Event required').isLength({min: 3, max: 80}).withMessage('Event Name' +
+		 ' must be longer than 2 characters and shorter than 81 characters.').trim(),
+	body('priority').optional({checkFalsy: true}).trim(),
+	body('eventDate').optional({checkFalsy: true}).isISO8601().withMessage('Valid Event date required'),
+	body('description').optional({checkFalsy: true}).trim(),
 	
-	let userid = '5a444deae24862450047baab';
+	sanitizeBody('name').trim().escape(),
+	sanitizeBody('priority').trim().escape(),
+	sanitizeBody('eventDate').trim().escape(),
+	sanitizeBody('description').trim().escape(),
 	
-	let userDate = req.body.date;
-	console.log(userDate);
-	//console.log(userDate.toUTCString());
-	
-	/*
-	let newEvent = new Event({
-		name: req.body.name,
-		user: userid,
-		priority: req.body.priority,
-		eventDate: req.body.date,
-		description: req.body.description,
-	});
-	
-	newEvent.save(function(error, savedEvent) {
-		if(error) {
-			console.log('Error at event save');
+	function(req, res, next) {
+		let errors = validationResult(req);
+		
+		if(errors.isEmpty()) {
+			let newEvent = new Event({
+				name: req.body.name,
+				user: '5a444deae24862450047baab',
+				priority: req.body.priority,
+				eventDate: req.body.date,
+				description: req.body.description,
+			});
+			
+			newEvent.save(function(error, savedEvent) {
+				if(error) { console.log('Error at event save'); } 
+				res.redirect(savedEvent.url);
+			});
+		} else {
+			res.render('error', {error: errors.array()});
+			return;
 		}
-		res.redirect(savedEvent.url);
-	});
-	*/
-}
+	}
+];
