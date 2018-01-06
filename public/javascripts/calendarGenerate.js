75dfe7851e0736f7c7d77d6d9baddf7e2f382485
ianH92 <>
2018-01-05 23:17:18 -0800

    fixed Event Style and added calendar functionality

--- public/javascripts/calendarGenerate.js
+++ public/javascripts/calendarGenerate.js
@@ -1,9 +1,120 @@
-let xmlReq = new XMLHttpRequest();
 let now = new Date();
 
+let xmlReq = new XMLHttpRequest();
 xmlReq.open('GET', ('/getcalendar/' + now.getFullYear() + '/' + now.getMonth()));
 xmlReq.responseType = 'json';
 xmlReq.send();
+
+let year = now.getFullYear();
+let month = now.getMonth();
+
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 
+			  'July', 'August', 'September', 'October', 'November', 'December'];
+let abbrMon = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+					'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+let pMonth = ((month - 1) === -1) ? 11 : (month - 1);
+let nMonth = ((month + 1) === 12) ?  0 : (month + 1);
+
+let prevMonth = document.getElementById('prevMonth');
+let currMonth = document.getElementById('currMonth');
+let nextMonth = document.getElementById('nextMonth');
+
+prevMonth.textContent = abbrMon[pMonth];
+currMonth.textContent =  months[month] + ' ' + year;
+nextMonth.textContent = abbrMon[nMonth];
+
+prevMonth.addEventListener('click', function() {
+	let xmlReq2 = new XMLHttpRequest();
+	month--;
+	month = (month === -1) ? 11 : month;
+	year = (month === 11) ? (year - 1) : year;
+	pMonth = (month === 0) ? 11 : (month - 1);
+	nMonth = (month === 11) ? 0 : (month + 1);
+	
+	xmlReq2.open('GET', ('/getcalendar/' + year + '/' + month));
+	xmlReq2.responseType = 'json';
+	xmlReq2.send();
+	
+	xmlReq2.onload = function() {
+		let cal = xmlReq2.response.calendar;
+		
+		for(let i = 0; i < 42; i++) {
+			let day = cal[i];
+			let td = document.getElementById(i + 1);
+			
+			while(td.firstChild) {
+				td.removeChild(td.firstChild);
+			}
+			
+			let p = document.createElement('p');
+			p.textContent = day['day'];
+			td.appendChild(p);
+			
+			for(let j = 0; j < day['events'].length; j++) {
+				let des = document.createElement('p');
+				des.textContent = day['events'][j].name;
+				td.appendChild(des);
+			}
+			
+			if(day['currMonth'] === true) {
+				td.setAttribute('class', 'full active');
+			} else {
+				td.setAttribute('class', 'full notactive');
+			}
+		}
+	}
+	
+	prevMonth.textContent = abbrMon[pMonth];
+	currMonth.textContent =  months[month] + ' ' + year;
+	nextMonth.textContent = abbrMon[nMonth];
+});
+
+nextMonth.addEventListener('click', function() {
+	let xmlReq2 = new XMLHttpRequest();
+	month++;
+	month = (month === 12) ? 0 : month;
+	year = (month === 0) ? (year + 1) : year;
+	pMonth = (month === 0) ? 11 : (month - 1);
+	nMonth = (month === 11) ? 0 : (month + 1);
+	
+	xmlReq2.open('GET', ('/getcalendar/' + year + '/' + month));
+	xmlReq2.responseType = 'json';
+	xmlReq2.send();
+	
+	xmlReq2.onload = function() {
+		let cal = xmlReq2.response.calendar;
+		
+		for(let i = 0; i < 42; i++) {
+			let day = cal[i];
+			let td = document.getElementById(i + 1);
+			
+			while(td.firstChild) {
+				td.removeChild(td.firstChild);
+			}
+			
+			let p = document.createElement('p');
+			p.textContent = day['day'];
+			td.appendChild(p);
+			
+			for(let j = 0; j < day['events'].length; j++) {
+				let des = document.createElement('p');
+				des.textContent = day['events'][j].name;
+				td.appendChild(des);
+			}
+			
+			if(day['currMonth'] === true) {
+				td.setAttribute('class', 'full active');
+			} else {
+				td.setAttribute('class', 'full notactive');
+			}
+		}
+	}
+	
+	prevMonth.textContent = abbrMon[pMonth];
+	currMonth.textContent =  months[month] + ' ' + year;
+	nextMonth.textContent = abbrMon[nMonth];
+});
 
 xmlReq.onload = function() {
 	let cal = xmlReq.response.calendar;
