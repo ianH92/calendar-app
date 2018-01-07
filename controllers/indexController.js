9950b9507b3c38c267246c4fa76456c4149e6a6f
ianH92 <>
2018-01-06 21:27:33 -0800

    added todo event views on home page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,11 @@
 var Events = require('../models/Event.js');
 
 exports.homePage = function(req, res) {
-	res.render('home', {title: 'Event Calendar'});
+	let user = '5a444deae24862450047baab';
+	
+	Events.find({user: user, eventDate: null}, 'name priority')
+	.sort({priority: +1, name: -1})
+	.exec(function(error, todoList) {
+		res.render('home', {title: 'Event Calendar', todos: todoList});
+	});
 }
